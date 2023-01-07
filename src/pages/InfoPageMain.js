0916ca0f22895d60df1c0de688acfd85a8d68f2d
Karolina Marszal <karomarszal@gmail.com>
2023-01-07 16:09:28 +0100

    Adds image

---
+++ src/pages/InfoPageMain.js
@@ -0,0 +1,9 @@
+import infotradeimg from "../images/infotradeimg.png"
+
+export const InfoPageMain = () => {
+  return <div>
+    <div className="text-center">
+      <img src={infotradeimg} alt="tradeimg"/>
+    </div>
+  </div>
+}
