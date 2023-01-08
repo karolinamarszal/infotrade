af77d909d1c60421cbb95d11937fd68e2d67944d
Karolina Marszal <karomarszal@gmail.com>
2023-01-08 19:50:12 +0100

    Adds StockList component

--- src/pages/InfoPageMain.js
+++ src/pages/InfoPageMain.js
@@ -1,9 +1,11 @@
 import infotradeimg from "../images/infotradeimg.png"
+import {StockList} from "../components/StockList"
 
 export const InfoPageMain = () => {
   return <div>
     <div className="text-center">
       <img src={infotradeimg} alt="tradeimg"/>
     </div>
+    <StockList />
   </div>
 }
