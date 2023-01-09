b25bb1124d855a67b2f5cc151774a5d35a1ebc34
Karolina Marszal <karomarszal@gmail.com>
2023-01-09 23:55:38 +0100

    Adds add, remove and local storage options

---
+++ src/context/searchListContext.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createContext, useState, useEffect } from "react";
+
+export const SearchListContext = createContext();
+
+export const SearchListContextProvider = (props) => {
+
+  const [searchList, setSearchList] = useState(
+    (localStorage.getItem("searchList") 
+    && localStorage.getItem("searchList").split(",") )
+    || ["GOOGL", "MSFT", "AMZN"]
+    // localStorage.getItem("searchList")?.split(",")|| ["GOOGL", "MSFT", "AMZN"]
+  )
+
+
+  useEffect(() => {
+    localStorage.setItem("searchList", searchList)
+  }, [searchList])
+
+  const addCompany = (stock) => {
+    if (searchList.indexOf(stock) === -1) {
+      setSearchList([...searchList, stock])
+    }
+  }
+
+  const deleteCompany = (stock) => {
+    setSearchList(searchList.filter((element) => {
+      return element !== stock
+    }))
+  }
+
+  return <SearchListContext.Provider value={{ searchList, addCompany, deleteCompany }}>
+    {props.children}
+  </SearchListContext.Provider>
+}
