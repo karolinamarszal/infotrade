d297951ca92d2188967e1b032b471c1f4f89bba2
Karolina Marszal <karomarszal@gmail.com>
2023-01-09 23:53:03 +0100

    Adds provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import "./App.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {InfoPageMain} from "./pages/InfoPageMain"
 import {InfoPageDetails} from "./pages/InfoPageDetails"
+import "./App.css";
+import { SearchListContextProvider } from "./context/searchListContext";
 
 function App() {
   return (
     <main className="container">
+      <SearchListContextProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<InfoPageMain />} />
           <Route path="/details/:symbol" element={<InfoPageDetails />} />
         </Routes>
       </BrowserRouter>
+      </SearchListContextProvider>
     </main>
   );
 }
