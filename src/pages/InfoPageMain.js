6a708a93918e48a209cead7ac306b2490fdcffb5
Karolina Marszal <karomarszal@gmail.com>
2023-01-09 23:55:56 +0100

    Adds autocomplete component

--- src/pages/InfoPageMain.js
+++ src/pages/InfoPageMain.js
@@ -1,11 +1,13 @@
 import infotradeimg from "../images/infotradeimg.png"
-import {StockList} from "../components/StockList"
+import { StockList } from "../components/StockList"
+import { AutoComplete } from "../components/AutoComplete"
 
 export const InfoPageMain = () => {
   return <div>
     <div className="text-center">
       <img src={infotradeimg} alt="tradeimg"/>
     </div>
+    <AutoComplete />
     <StockList />
   </div>
 }
