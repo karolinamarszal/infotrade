6a9817cc22751637bfb088c75d1151334a936a81
Karolina Marszal <karomarszal@gmail.com>
2023-01-11 17:47:16 +0100

    Adds navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {InfoPageMain} from "./pages/InfoPageMain"
 import {InfoPageDetails} from "./pages/InfoPageDetails"
+import { NavBar } from "./components/NavBar"
 import "./App.css";
 import { SearchListContextProvider } from "./context/searchListContext";
 
 function App() {
   return (
-    <main className="container">
-      <SearchListContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<InfoPageMain />} />
-          <Route path="/details/:symbol" element={<InfoPageDetails />} />
-        </Routes>
-      </BrowserRouter>
-      </SearchListContextProvider>
-    </main>
-  );
+    <div>
+      <NavBar />
+      <main className="container">
+        <SearchListContextProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<InfoPageMain />} />
+              <Route path="/detail/:symbol" element={<InfoPageDetails />} />
+            </Routes>
+          </BrowserRouter>
+        </SearchListContextProvider>
+      </main>
+    </div>
+    );
 }
 
 export default App;
