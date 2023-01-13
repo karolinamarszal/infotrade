62bd0c859dd36fe7d09011a971e30a3ef48f2ef1
Karolina Marszal <karomarszal@gmail.com>
2023-01-13 17:59:36 +0100

    Styles image

--- src/pages/InfoPageMain.js
+++ src/pages/InfoPageMain.js
@@ -1,11 +1,12 @@
-import infotradeimg from "../images/infotradeimg.png"
-import { StockList } from "../components/StockList"
-import { AutoComplete } from "../components/AutoComplete"
+import infotradeimg from "../images/infotradeimg.png";
+import { StockList } from "../components/StockList";
+import { AutoComplete } from "../components/AutoComplete";
+
 
 export const InfoPageMain = () => {
   return <div>
     <div className="text-center">
-      <img src={infotradeimg} alt="tradeimg"/>
+      <img src={infotradeimg} style={{width: "15%"}} alt="tradeimg"/>
     </div>
     <AutoComplete />
     <StockList />
