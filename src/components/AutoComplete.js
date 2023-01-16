c0239742bb5412804cd66a16f04e37679577542f
Karolina Marszal <karomarszal@gmail.com>
2023-01-16 19:35:13 +0100

    Moved style to css class

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -57,7 +57,7 @@ export const AutoComplete = () => {
 
   return <div className="w-50 p-5 rounded mx-auto">
     <div className="form-floating dropdown">
-      <input style={{backgroundColor: "rgba(145, 158, 171, 0.7"}} id="search" type="text" className="form-control"
+      <input id="search" type="text" className="form-control"
       placeholder="Search" autoComplete="off" value={search}
       onChange={(e) =>
       setSearch(e.target.value)}></input>
