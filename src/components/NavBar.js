b2444cf7448bd3741b4d433fe8888aa436f02363
Karolina Marszal <karomarszal@gmail.com>
2023-01-17 19:15:26 +0100

    Adds css classes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,8 +16,8 @@ export const NavBar = () => {
   }
 
 
-  return <header className="d-flex justify-content-between align-items-center mb-4 px-2 shadow " style={{height: "50px", backgroundColor: "#f0f2f5"}}>
-    <button className="btn-nav" style={{height: "40px"}} onClick={home}>HOME</button>
+  return <header className="d-flex justify-content-between align-items-center mb-4 px-2 shadow nav home">
+    <button className="btn-nav home" onClick={home}>HOME</button>
     <div className="switch">
       <label style={{marginRight: "10px"}}> { theme === "light" ? "Light Mode" : "Dark Mode"} </label>
       <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
